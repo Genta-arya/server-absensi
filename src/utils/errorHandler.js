0e7e25507a9233280a8f4304d460e5e7b8e60155
Genta-arya <mgentaarya@gmail.com>
2024-11-03 16:17:01 +0700

    validasi grup id

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,8 +1,21 @@
-export const handleError = (
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export const handleError = async (
   res,
   error,
   customMessage = "Terjadi kesalahan pada server"
 ) => {
   console.error(error);
+
+  // Simpan log error ke database
+  await prisma.errorLog.create({
+    data: {
+      message: customMessage,
+      statusCode: 500,
+      detail: error,
+    },
+  });
+
   return res.status(500).json({ message: customMessage, Detail: error });
 };
--- src/utils/responseHandler.js
+++ src/utils/responseHandler.js
@@ -1,14 +1,26 @@
 // utils/responseHandler.js
-export const sendResponse = (res, statusCode, message, data = null) => {
-    const responsePayload = {
-      message,
-    };
-  
-    
-    if (data) {
-      responsePayload.data = data;
-    }
-  
-    return res.status(statusCode).json(responsePayload);
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export const sendResponse = async (res, statusCode, message, data = null) => {
+  const responsePayload = {
+    message,
   };
-  
+
+  if (data) {
+    responsePayload.data = data;
+  }
+
+  // Simpan log ke database jika status code bukan 200 (misalnya untuk status error atau informasi penting lainnya)
+  if (statusCode === 200) {
+    await prisma.errorLog.create({
+      data: {
+        message: message,
+        detail: "sukses",
+        statusCode: 200,
+      },
+    });
+  }
+
+  return res.status(statusCode).json(responsePayload);
+};
