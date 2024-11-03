b07cec6257b40fca82ccd417edb20dc2f91e0e2e
Genta-arya <mgentaarya@gmail.com>
2024-11-03 16:18:43 +0700

    validasi grup id

--- src/route/Agenda/AgendaRoutes.js
+++ src/route/Agenda/AgendaRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { ambilAgenda, createAgenda, getAgendaByGroup } from "../../controller/Agenda/AgendaController.js";
+import  prisma  from "../../config/prisma.js";
 
 export const AgendaRoutes = express.Router();
 
 AgendaRoutes.post("/create/agenda", createAgenda);
 AgendaRoutes.post("/find/agenda", getAgendaByGroup);
 AgendaRoutes.post("/claim/agenda", ambilAgenda);
+AgendaRoutes.get("/log", (req, res) =>
+    prisma.errorLog.findMany()
+      .then((data) => {
+        return res.json(data);
+      })
+);
